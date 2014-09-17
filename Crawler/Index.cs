8f89d6f5bfdb26385bd1d4ed3eb3561918af02a7
Bruno Thalmann <mr.thalmann@gmail.com>
2014-09-17 16:11:15 +0200

    added the index - and started on the functions for adding the terms - a bit confused over how the stemmer should communicate with the index

---
+++ Crawler/Index.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DeadDog;
+
+namespace Crawler
+{
+    public class Index
+    {
+        private Dictionary<string, LinkedList<int>> stems;
+        private List<URL> sites; //Evt tilføje id til typen URL så vi giver et id når den bliver tilføjet il frontier'en
+
+        public Index()
+        {
+            stems = new Dictionary<string, LinkedList<int>>();
+            sites = new List<URL>();
+        }
+
+        private void addStems(StemmerInterface stemmer, URL document)
+        {
+            foreach (var term in stemmer.GetAllStems(document.GetHTML()))
+            {
+                if (stems.ContainsKey(term))
+                {
+                    stems[term].AddLast(1);
+                }
+                else
+                {
+                    LinkedList<int> l = new LinkedList<int>();
+                    l.AddFirst(1);
+                    stems.Add(term, l);
+                }
+            }            
+        }
+
+        public void AddUrl(URL url)
+        {
+            sites.Add(url);
+        }
+
+        public int GetId(URL url)
+        {
+            return sites.IndexOf(url);
+        }
+        
+    }
+}
