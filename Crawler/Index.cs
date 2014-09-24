a95ffa86372defd27e7763242b59e4454295a865
deaddog <mikkelslsl@gmail.com>
2014-09-24 15:31:34 +0200

    Revert "tilføjet en indkapslet type der indeholder URL'erne"
    This reverts commit 67442c8c63a231ec3fbfcb92e10cb236de6b8c15.

--- Crawler/Index.cs
+++ Crawler/Index.cs
@@ -10,66 +10,40 @@ namespace Crawler
     public class Index
     {
         private Dictionary<string, LinkedList<int>> stems;
+        private List<URL> sites; //Evt tilføje id til typen URL så vi giver et id når den bliver tilføjet il frontier'en
 
         public Index()
         {
             stems = new Dictionary<string, LinkedList<int>>();
-            sites = new SitesCollection<URL>();
+            sites = new List<URL>();
         }
 
-        public void AddStems(StemmerInterface stemmer, string document, URL url)
+        private void addStems(StemmerInterface stemmer, URL document)
         {
-            foreach (var term in stemmer.GetAllStems(document))
+            foreach (var term in stemmer.GetAllStems(document.GetHTML()))
             {
                 if (stems.ContainsKey(term))
                 {
-                    stems[term].AddLast(sites.Count + 1);
+                    stems[term].AddLast(1);
                 }
                 else
                 {
                     LinkedList<int> l = new LinkedList<int>();
-                    l.AddFirst(sites.Count + 1);
+                    l.AddFirst(1);
                     stems.Add(term, l);
                 }
-            }
+            }            
+        }
+
+        public void AddUrl(URL url)
+        {
             sites.Add(url);
         }
 
-        private SitesCollection<URL> sites;
-
-        public SitesCollection<URL> Sites
+        public int GetId(URL url)
         {
-            get { return sites; }
+            return sites.IndexOf(url);
         }
-
-        public int Count { get { return sites.Count; } }
-
-        public class SitesCollection<T> : IEnumerable<T>
-        {
-            private List<T> sites;
-
-            public SitesCollection()
-            {
-                this.sites = new List<T>();
-            }
-
-            public int Count { get { return sites.Count; } }
-
-            public void Add(T item)
-            {
-                sites.Add(item);
-            }
-
-            public IEnumerator<T> GetEnumerator()
-            {
-                return sites.GetEnumerator();
-            }
-
-            System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
-            {
-                return sites.GetEnumerator();
-            }
-        }
-
+        
     }
 }
