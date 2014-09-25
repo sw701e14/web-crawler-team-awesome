644e4209a5ceff6c1489d23b52409261c8474cdc
deaddog <mikkelslsl@gmail.com>
2014-09-25 17:25:44 +0200

    Copied the main part of Program into the Crawler class.

--- Crawler/Crawler.cs
+++ Crawler/Crawler.cs
@@ -10,6 +10,61 @@ namespace WebCrawler
 {
     public class Crawler
     {
+        private ThreadedFrontier frontier;
+        private Index index;
+        private Filtering.Filter filter;
+        private ISimilarityComparer<Document> similarity;
+
+        private Crawler(ThreadedFrontier frontier, Index index, Filtering.Filter filter, ISimilarityComparer<Document> similarity)
+        {
+            this.frontier = frontier;
+            this.index = index;
+            this.filter = filter;
+            this.similarity = similarity;
+        }
+
+        private void Run()
+        {
+            Document doc = frontier.Next();
+            while (doc != null)
+            {
+                Console.WriteLine("{0}", doc.URL);
+                Console.WriteLine("Loading {0} shingles...", doc.HTML.Split(' ').Length);
+                similarity.LoadShingles(doc, doc.HTML);
+
+                Console.WriteLine("Determining similarities...");
+                bool known = false;
+                foreach (var l in index.GetDocuments())
+                {
+                    double simi = similarity.CalculateSimilarity(l, doc);
+                    if (simi >= 0.9)
+                    {
+                        WriteColorLine("{0:0.0}% similar to {1}", ConsoleColor.Green, simi * 100, l.URL.Address);
+                        known = true;
+                        break;
+                    }
+                }
+
+                if (!known)
+                {
+                    index.AddUrl(doc);
+                    Console.WriteLine("Extracting links...");
+                    var links = GetLinks(doc.URL, doc.HTML).ToArray();
+
+                    int c = 0;
+                    foreach (var l in links)
+                        if (filter.Allow(l))
+                        {
+                            frontier.Add(l);
+                            c++;
+                        }
+                    WriteColorLine("Found {0} links, added {1} to frontier", ConsoleColor.Cyan, links.Length, c);
+                }
+                Console.WriteLine();
+                doc = frontier.Next();
+            }
+        }
+
         private static IEnumerable<URL> GetLinks(URL origin, string html)
         {
             var matches = Regex.Matches(html, "<a[^>]+");
