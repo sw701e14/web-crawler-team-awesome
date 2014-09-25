41f29300750c8a9767b7d9cdf5bc7d7967573a17
deaddog <mikkelslsl@gmail.com>
2014-09-25 15:03:14 +0200

    Copied the GetLinks method from Program to the Crawler.

--- Crawler/Crawler.cs
+++ Crawler/Crawler.cs
@@ -1,13 +1,56 @@
-﻿using System;
+﻿using DeadDog;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
 namespace WebCrawler
 {
     public class Crawler
     {
-        
+        private static IEnumerable<URL> GetLinks(URL origin, string html)
+        {
+            var matches = Regex.Matches(html, "<a[^>]+");
+
+            for (int i = 0; i < matches.Count; i++)
+            {
+                string url = matches[i].Value;
+
+                url = getUrl(url);
+
+                if (url != null)
+                {
+                    try { yield return origin.GetURLFromLink(url); }
+                    finally { /* This is only here so that try is allowed */ }
+                }
+            }
+        }
+        private static string getUrl(string anchor)
+        {
+            Match m;
+            m = Regex.Match(anchor, "href=\"(?<url>[^\"]+)");
+            if (!m.Success)
+                return null;
+
+            string href = m.Groups["url"].Value;
+            m = Regex.Match(href, "(?<pretag>[^#]*)#[^#]*");
+            if (m.Success)
+                href = m.Groups["pretag"].Value;
+
+            if (href.Length == 0)
+                return null;
+
+            return href;
+        }
+
+        private static void WriteColorLine(string text, ConsoleColor color, params object[] args)
+        {
+            ConsoleColor temp = Console.ForegroundColor;
+            Console.ForegroundColor = color;
+            Console.WriteLine(text, args);
+            Console.ForegroundColor = temp;
+        }
     }
 }
