0868f7b4a4c78bdcfd6b0d43f15c1bb0de80558c
deaddog <mikkelslsl@gmail.com>
2014-10-01 14:37:45 +0200

    Removed the GetLinks method from Program.

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -36,41 +36,6 @@ namespace WebCrawler
             frontier.Kill();
         }
 
-        private static IEnumerable<URL> GetLinks(URL origin, string html)
-        {
-            var matches = Regex.Matches(html, "<a[^>]+");
-
-            for (int i = 0; i < matches.Count; i++)
-            {
-                string url = matches[i].Value;
-
-                url = getUrl(url);
-
-                if (url != null)
-                {
-                    try { yield return origin.GetURLFromLink(url); }
-                    finally { /* This is only here so that try is allowed */ }
-                }
-            }
-        }
-        private static string getUrl(string anchor)
-        {
-            Match m;
-            m = Regex.Match(anchor, "href=\"(?<url>[^\"]+)");
-            if (!m.Success)
-                return null;
-
-            string href = m.Groups["url"].Value;
-            m = Regex.Match(href, "(?<pretag>[^#]*)#[^#]*");
-            if (m.Success)
-                href = m.Groups["pretag"].Value;
-
-            if (href.Length == 0)
-                return null;
-
-            return href;
-        }
-
         private static void WriteColorLine(string text, ConsoleColor color, params object[] args)
         {
             ConsoleColor temp = Console.ForegroundColor;
