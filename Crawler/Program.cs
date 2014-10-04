3ae8bab3b1a94e0255c8929f629758add379a864
deaddog <mikkelslsl@gmail.com>
2014-10-04 13:42:05 +0200

    Included the new ranker call and updated the console feedback. Also added multiple searches to console.

--- Crawler/Program.cs
+++ Crawler/Program.cs
@@ -30,18 +30,33 @@ namespace WebCrawler
             Crawler.StartAndWait(frontier, index, filter, 100);
 
             DateTime end = DateTime.Now;
-            Console.WriteLine("Done in {0}", (end - start).TotalSeconds);
-
-            Ranker r = new Ranker(index, new PorterStemmer());
-            Console.WriteLine("Search: ");
-            string searchQuery = Console.ReadLine();
-            foreach (var doc in r.GetTopHits(searchQuery))
-            {
-                Console.WriteLine(doc.Key.URL + " " + doc.Value);
-            }
-            Console.ReadKey();
-
+            Console.WriteLine("Crawler done in {0:0.00} sec ({1:0.00} pages per sec).", (end - start).TotalSeconds, index.SiteCount / (end - start).TotalSeconds);
+            Console.WriteLine("Press any key to start querying.");
             Console.ReadKey(true);
+            Console.WriteLine();
+
+            start = DateTime.Now;
+            Ranker r = new Ranker(index, TrimmingStemmer.GetStemmer(PorterStemmer.StemTerm));
+            end = DateTime.Now;
+            Console.WriteLine("Ranker created in {0:0.00} sec.", (end - start).TotalSeconds);
+
+            string searchQuery = "";
+            while (true)
+            {
+                Console.WriteLine("Query for data below. Enter an empty string to quit.");
+                Console.Write("Search for: ");
+                searchQuery = Console.ReadLine();
+                if (searchQuery == "")
+                    break;
+
+                start = DateTime.Now;
+                foreach (var doc in r.GetHits(searchQuery).OrderByDescending(x => x.Item2))
+                    Console.WriteLine("Rank: {1:0.000000} for: {0}", doc.Item1.URL.Address, doc.Item2);
+                end = DateTime.Now;
+
+                Console.WriteLine("Query completed in {0:0.00} sec", (end - start).TotalSeconds);
+                Console.WriteLine();
+            }
             frontier.Kill();
         }
 
